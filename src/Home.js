23ca6f78e10be410459d9c8cda932ca6f3806d1e
Malinchristina <malin_christina@yahoo.se>
2024-10-07 19:56:42 +0200

    play in the home file

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,12 @@
 const Home = () => {
+    const handleClick = () => {
+      console.log('Hello, ninjas', 'This is a test');
+    }
+      
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click me</button>
         </div>
     );
 }
